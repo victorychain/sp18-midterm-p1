67c0f80604dd61b9fc68a6f562890b8ca4861ba3
Aditya Srivastava <aditya.srivastava@berkeley.edu>
2018-03-01 23:59:54 -0800

    more development of crowdsale

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -13,25 +13,61 @@ contract Crowdsale {
 
   address public owner;
   uint public initialTokens;
-  uint public tokensToWei;
+  uint public tokensPerWei;
   uint private totalFunds;
+  uint public startTime;
+  uint public endTime;
+  uint public tokensSold;
 
   modifier ownerOnly() {
     require(msg.sender == owner);
     _;
   }
 
-  function Crowdsale(uint initialTokens, uint tokensToWei) {
+  modifier timeConstraint() {
+      require(now > startTime && now < endTime);
+      _;
+  }
+
+   event TokenPurchased(address buyer);
+   event TokenSold(address seller);
+
+  function Crowdsale(uint _initialTokens, uint _tokensPerWei, uint duration) {
     owner = msg.sender;
+    startTime = now;
+    endTime = startTime + duration;
+    tokensPerWei = _tokensPerWei;
+    initialTokens = _initialTokens;
+
   }
 
   function mintTokens(uint amount) ownerOnly {
-
+    //add to totalSupply in Token.sol
   }
 
   function burnTokens(uint amount) ownerOnly {
-
+    //subtract from totalSupply in Token.sol
   }
 
+  function buyTokens(uint amount) public timeConstraint {
+    //increment token balance for msg.sender in Token.sol
+    tokensSold += amount;
+    TokenPurchased(msg.sender);
+  }
+
+  function refundTokens(uint amount) public timeConstraint {
+    //decrement token balance for msg.sender in Token.sol
+    tokensSold -= amount;
+    TokenSold(msg.sender);
+  }
+
+  function timeRemaining() view returns (uint) {
+    return endTime - startTime;
+  }
+
+  function sendOwnerFunds() ownerOnly {
+    require(now > endTime);
+    owner.transfer(totalFunds);
+  }
 
 }
